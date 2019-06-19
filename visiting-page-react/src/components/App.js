d59b066508b29cb0c449061e5b677ebac9358918
Marta Kołodziej <marta.koodziej@gmail.com>
2019-06-19 21:27:50 +0200

    Create HomePage component

--- visiting-page-react/src/components/App.js
+++ visiting-page-react/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import HomePage from './HomePage'
 
 export default class App extends React.Component {
     render() {
-        return <div>App</div>
+        return <div><HomePage /></div>
     }
 }
