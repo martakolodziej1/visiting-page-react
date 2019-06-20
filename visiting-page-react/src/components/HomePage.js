f33f53aefdb717820e64d662c4d6bbf1a7f2b656
Marta Kołodziej <marta.koodziej@gmail.com>
2019-06-20 17:22:44 +0200

    Add basic styles

--- visiting-page-react/src/components/HomePage.js
+++ visiting-page-react/src/components/HomePage.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import '../style/style.css';
 
+const imgProfile = {
+    src: process.env.PUBLIC_URL + '/img/profile.jpg',
+    alt: 'profile'
+};
+
 export default class HomePage extends React.Component {
     render() {
-        return <div>Home page</div>
+        return ( 
+            <div className="container">
+                <img src={imgProfile.src} alt={imgProfile.alt} />
+                <hr></hr>
+                <h1>Hello!</h1>
+                <h2>My name is Marta Kolodziej</h2>
+                <h3>I am a front-end developer</h3>
+            </div>
+        )
     }
 }
