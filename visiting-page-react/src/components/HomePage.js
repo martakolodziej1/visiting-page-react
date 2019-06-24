669b4c62f6266e43a43d0138a336b288172b7456
Marta Kołodziej <marta.koodziej@gmail.com>
2019-06-24 20:40:04 +0200

    Add navbar and new styles

--- visiting-page-react/src/components/HomePage.js
+++ visiting-page-react/src/components/HomePage.js
@@ -10,11 +10,20 @@ export default class HomePage extends React.Component {
     render() {
         return ( 
             <div className="container">
-                <img src={imgProfile.src} alt={imgProfile.alt} />
-                <hr></hr>
-                <h1>Hello!</h1>
-                <h2>My name is Marta Kolodziej</h2>
-                <h3>I am a front-end developer</h3>
+                <div className="nav">
+                    <button>Home</button>
+                    <button>About me</button>
+                    <button>Skills</button>
+                    <button>Portfolio</button>
+                    <button>Contact</button>
+                </div>
+                <div className="box">
+                    <img src={imgProfile.src} alt={imgProfile.alt} />
+                    <hr></hr>
+                    <h1>Hello!</h1>
+                    <h2>My name is Marta Kolodziej</h2>
+                    <h3>I am a front-end developer</h3>
+                </div>
             </div>
         )
     }
