ff993a9405aebd3c656286a1671431f73096c950
Marta Kołodziej <marta.koodziej@gmail.com>
2019-06-27 21:33:17 +0200

    Create ThankYou component with styles

---
+++ visiting-page-react/src/components/ThankYou.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import HomePage from './HomePage';
+import '../style/style.css';
+
+export default class ThankYou extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            goToHomePage: false
+        };
+    }
+
+    jumpToHomePage = () => {
+        this.setState({goToHomePage: true});
+    }
+
+    render () {
+        if (!this.state.goToHomePage)
+            return (
+                <div className="container">
+                    <div className="box">
+                        <div className="thanks">
+                            <h1>Thank you for getting in touch!</h1>
+                            <p>I appreciate you contacting me.<br></br>I will answer you as soon as possible.</p>
+                            <h2>Have a great day!</h2>
+                            <button onClick={this.jumpToHomePage}>Home</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        else return (<HomePage />)
+    }
+}
