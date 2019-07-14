7b5b800537b3fb5c1f8a79135b03e556f1e1657f
martakolodziej1 <marta.koodziej@gmail.com>
2019-07-14 13:33:10 +0200

    State modification in main page for cards switching

--- visiting-page-react/src/components/HomePage.js
+++ visiting-page-react/src/components/HomePage.js
@@ -12,38 +12,39 @@ export default class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            goToAboutMe: false,
-            goToSkills: false
+            goTo: ""
         };
     }
 
-    // jumpToAboutMe = () => {
-    //     this.setState({goToAboutMe: true});
-    // }
+     jumpToAboutMe = () => {
+         this.setState({goTo: "AboutMe"});
+     }
 
     jumpToSkills = () => {
-        this.setState({goToSkills: true});
+        this.setState({goTo: "Skills"});
     }
 
     render() {
-        if (!this.state.goToSkills)
-            return (
-                <div className="container">
-                    <div className="nav">
-                        <button>Home</button>
-                        <button onClick={this.jumpToAboutMe}>About me</button>
-                        <button onClick={this.jumpToSkills}>Skills</button>
-                        <button>Contact</button>
-                    </div>
-                    <div className="box">
-                        <img src={imgProfile.src} alt={imgProfile.alt} />
-                        <hr></hr>
-                        <h1>Hello!</h1>
-                        <h2>My name is Marta Kolodziej</h2>
-                        <h3>I am a front-end developer</h3>
-                    </div>
+        if (this.state.goTo === "Skills")
+            return (<Skills />)
+        if (this.state.goTo === "AboutMe")
+            return (<AboutMe />)
+        return (
+            <div className="container">
+                <div className="nav">
+                    <button>Home</button>
+                    <button onClick={this.jumpToAboutMe}>About me</button>
+                    <button onClick={this.jumpToSkills}>Skills</button>
+                    <button>Contact</button>
                 </div>
-            )
-        else return (<Skills />)
+                <div className="box">
+                    <img src={imgProfile.src} alt={imgProfile.alt} />
+                    <hr></hr>
+                    <h1>Hello!</h1>
+                    <h2>My name is Marta Kolodziej</h2>
+                    <h3>I am a front-end developer</h3>
+                </div>
+            </div>
+        )
     }
 }
