c5353dcf5ccb94b288855f56b43b975c7587b604
Marta <marta.koodziej@gmail.com>
2019-08-19 20:56:36 +0200

    Updates

---
+++ precache-manifest.d90800f05df5f4ad6916c0c5efa68b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff13e537ae02ddc7787cad8c1bc9cd47",
+    "url": "/visiting-page-react/index.html"
+  },
+  {
+    "revision": "7f681bf94c5725aade80",
+    "url": "/visiting-page-react/static/css/main.085b602f.chunk.css"
+  },
+  {
+    "revision": "22227e619576f5be343a",
+    "url": "/visiting-page-react/static/js/2.64918525.chunk.js"
+  },
+  {
+    "revision": "7f681bf94c5725aade80",
+    "url": "/visiting-page-react/static/js/main.4e0ad4b9.chunk.js"
+  },
+  {
+    "revision": "3c389878a13de4741ee2",
+    "url": "/visiting-page-react/static/js/runtime~main.7f37ee06.js"
+  }
+]);
