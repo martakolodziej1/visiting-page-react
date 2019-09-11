9e17142e9564afc539642b3c099e904213bfbc74
Marta <marta.koodziej@gmail.com>
2019-09-11 22:14:29 +0200

    Add inline styles to the Image component

--- visiting-page-react/src/components/Image.js
+++ visiting-page-react/src/components/Image.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../style/style.css';
 
-const image = {
-    src: process.env.PUBLIC_URL + '/img/stil-xhb7s-Q-ECA-unsplash.jpg',
-    alt: 'desk image'
+const imageUrl = '/img/stil-xhb7s-Q-ECA-unsplash.jpg'
+
+const imageStyle = {
+    backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)), url(' + imageUrl + ')',
+    backgroundSize: 'cover'
 };
 
 export default class Image extends React.Component{
     render() {
-        return <img src={image.src} alt={image.alt}/>
+        return <img style={ imageStyle }/>
     }
 }
