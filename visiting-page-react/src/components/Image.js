0fe23b9504b13639a10c11e9ff65574c58ab8c52
Marta <marta.koodziej@gmail.com>
2019-09-15 16:45:24 +0200

    Lacking image on github fix

--- visiting-page-react/src/components/Image.js
+++ visiting-page-react/src/components/Image.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/style.css';
 
-const imageUrl = '/img/stil-xhb7s-Q-ECA-unsplash.jpg'
+const imageUrl = './img/stil-xhb7s-Q-ECA-unsplash.jpg'
 
 const imageStyle = {
     backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)), url(' + imageUrl + ')',
